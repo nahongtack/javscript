e6f2f502252ec57b2b90f831bcbf6fc3ad2e1418
hongtaek na <nahongtack@gmail.com>
2022-12-18 18:08:31 +0900

    feat : Javascript calculator app

---
+++ app.js
@@ -0,0 +1,27 @@
+// create calculator object
+const calculator = {
+    add : function (a, b) { // 더하기
+        return a + b;
+    },
+    minus : function (a, b) { // 빼기
+        return a - b;
+    },
+    multiply : function (a, b) { // 곱하기
+        return a * b;
+    },
+    divide : function (a, b) { // 나누기
+        return a / b;
+    },
+    square : function (a, b){ // 제곱
+        return a**b;
+    }
+}
+
+const firstNumber = 2;
+const secondNumber = 5;
+
+calculator.add(firstNumber, secondNumber);
+calculator.minus(firstNumber, secondNumber);
+calculator.multiply(firstNumber, secondNumber);
+calculator.divide(firstNumber, secondNumber);
+calculator.square(firstNumber, secondNumber);
